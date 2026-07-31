4df5251cbf73f6f4242de0eafa3496053e996862
Suphichaya0311 <oil.0877611417@gmail.com>
2026-07-31 18:14:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,106 @@
 
+// Loading Screen
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("loading-screen").style.display = "none";
+    }, 1800);
+});
+
+// Password
+
+function checkPassword() {
+
+    const password =
+        document.getElementById("passwordInput").value;
+
+    if(password === "031120"){
+
+        document
+            .getElementById("password-screen")
+            .classList.remove("active");
+
+        document
+            .getElementById("gift-screen")
+            .classList.add("active");
+
+    }else{
+
+        alert("Wrong Password ❤️");
+
+    }
+}
+
+// Music
+
+const music =
+    document.getElementById("bgMusic");
+
+const musicBtn =
+    document.getElementById("musicBtn");
+
+let isPlaying = false;
+
+musicBtn.addEventListener("click",()=>{
+
+    if(isPlaying){
+
+        music.pause();
+        isPlaying = false;
+
+    }else{
+
+        music.play();
+        isPlaying = true;
+
+    }
+
+});
+
+// Gift Open
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+    const gift =
+        document.getElementById("giftBox");
+
+    if(gift){
+
+        gift.addEventListener("click",()=>{
+
+            gift.innerHTML = "💖";
+
+            music.play();
+            isPlaying = true;
+
+            setTimeout(()=>{
+
+                document
+                    .getElementById("gift-screen")
+                    .classList.remove("active");
+
+                document
+                    .getElementById("welcome-screen")
+                    .classList.add("active");
+
+            },1000);
+
+        });
+
+    }
+
+});
+
+// Welcome -> Timeline
+
+function goToTimeline(){
+
+    document
+        .getElementById("welcome-screen")
+        .classList.remove("active");
+
+    document
+        .getElementById("timeline-screen")
+        .classList.add("active");
+
+}
